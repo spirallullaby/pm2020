25ee578a93c421be34fb73d5c30103013333f33a
spirallullaby <ralicatst@gmail.com>
2020-12-03 16:01:07 +0200

    Lecture 5 - templates, interpolation, directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductListComponent],
+  bootstrap: [AppComponent, ProductListComponent]
 })
 export class AppModule { }
