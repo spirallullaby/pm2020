000662a253910af8c16a5003a958936459f379cf
spirallullaby <ralicatst@gmail.com>
2020-12-11 11:08:59 +0200

    Lecture 11 - navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,23 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star-component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule],
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      { path: 'product/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent, WelcomeComponent],
   bootstrap: [AppComponent],
   providers: [ProductListComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,27 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import { ProductService } from './products.service';
+import { ProductService} from './products.service';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css'],
-    providers: [ProductListComponent]
+    styleUrls: ['./product-list.component.css']
 })
-
 export class ProductListComponent implements OnInit {
     pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     filteredProducts: IProduct[]; 
-    products: IProduct[];
+    products: IProduct[] = [];
     errorMessage: string;
-
     _listFilter: string;
 
     constructor(private _productService: ProductService) {
-        this.filteredProducts = this.products;
+        this.listFilter = '';
     }
 
     get listFilter(): string {
@@ -41,14 +38,16 @@ export class ProductListComponent implements OnInit {
     
 
     ngOnInit(): void {
+        console.log('In OnInit');
+ 
         this._productService.getProducts()
-        .subscribe({
-            next: products => {
-                this.products = products;
-                this.filteredProducts = this.products;
-            },
-            error: error => this.errorMessage = <any>error
-        });
+            .subscribe({
+                next: products => {
+                    this.products = products;
+                    this.filteredProducts = this.products;
+                },
+                error: error => this.errorMessage = <any>error
+            }); 
     }
 
     toggleImage(): void {
